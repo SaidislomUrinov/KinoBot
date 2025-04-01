d3a2963b6e7e6a3cf68a8301ed6a1f2b966cfaee
SaidislomUrinov <me@saidnet.uz>
2025-04-01 19:25:10 +0500

    configs

--- server/src/utils/configs.js
+++ server/src/utils/configs.js
@@ -1,7 +1,7 @@
 export default {
     port: 5000,
-    token: '7953101279:AAF0x145v7_tYFTsUHicTFyxi-I3ZtnYWEw',
+    token: 'token',
     mongoUrl: 'mongodb://127.0.0.1:27017/kino',
     admins: [5991285234],
-    access: 'sasasasd65f67g89jio'
+    access: 'jwt_access'
 }
